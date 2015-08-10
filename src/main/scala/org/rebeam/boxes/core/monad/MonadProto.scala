ea1dd5741dcf6bcb2f4138db9b08d60f3bc0c368
trepidacious <trepidacious@gmail.com>
2015-08-10 11:42:53 +0100

    Make Shelf a singleton for monadic implementation
    This removes path-dependent types and simplifies code quite a bit,
    removing need for an implicit Shelf. It shouldn't greatly reduce
    utility, since any use cases for multiple shelves are probably edge
    cases.
    
    Renamed classes to remove "Monad" and take everything into monad
    package for prototyping. Added functions to repeatedly run States until
    they produce a successful result - equivalent of "transact" in older
    code.

--- src/main/scala/org/rebeam/boxes/core/monad/MonadProto.scala
+++ src/main/scala/org/rebeam/boxes/core/monad/MonadProto.scala
@@ -3,20 +3,162 @@ package org.rebeam.boxes.core.monad
 import java.util.concurrent.atomic.AtomicInteger
 
 import org.rebeam.boxes.core.util.{RWLock, GCWatcher, BiMultiMap}
-import org.rebeam.boxes.core._
+
+import org.rebeam.boxes.core.Identifiable
 
 import scala.collection.immutable._
-import scala.collection.mutable.WeakHashMap
 import scalaz.State
 
-class ShelfMonad() {
+case class BoxChange(revision: Long)
 
-  def create[T](t: T): State[this.RevisionDelta, Box[T]] = State(_.create(t))
-  def set[T](box: Box[T], t: T): State[this.RevisionDelta, Box[T]] = State(_.set(box, t))
-  def get[T](box: Box[T]): State[this.RevisionDelta, T] = State(_.get(box))
+class Box[T](val id: Long) extends Identifiable {
+  /**
+   * Store changes to this box, as a map from the Change to the State that was
+   * set by that Change. Changes that form a revision are retained by RevisionMonad
+   * and used to look up States in refs.
+   * Since this is a weak map, it does not retain Changes, they are retained only
+   * by the revisions they are in.
+   * When a Box is GCed, the changes are also GCed, allowing the States
+   * of the Box (instances of T) to be removed.
+   */
+  private val changes = new scala.collection.mutable.WeakHashMap[BoxChange, T]()
+
+  private[core] def addChange(c: BoxChange, t: T) = changes.put(c, t)
+
+  private[core] def getValue(c: BoxChange) = changes.get(c)
+
+  override def toString = "Box(id = " + id + ", changes = " + changes + ")"
+}
+
+object Box {
+  private val nextId = new AtomicInteger(0)
+  def apply[T](): Box[T] = new Box[T](nextId.getAndIncrement())
+}
+
+case class BoxState[+T](revision: Long, value: T)
+
+case class Reaction(id: Long) extends Identifiable
+
+object Reaction {
+  private val nextId = new AtomicInteger(0)
+  def apply(): Reaction = Reaction(nextId.getAndIncrement())
+}
+
+case class RevisionDelta(
+                          base: Revision,
+                          creates: Set[Box[_]],
+                          reads: Set[Long],
+                          writes: Map[Box[_], _],
+                          newReactions: Map[Reaction, State[RevisionDelta, Unit]],
+                          sources: BiMultiMap[Long, Long],
+                          targets: BiMultiMap[Long, Long],
+                          boxReactions: Map[Long, Set[Reaction]]) {
+
+  def create[T](t: T) = {
+    val box = Box[T]()
+    (this.copy(creates = creates + box).copy(writes = writes.updated(box, t)), box)
+  }
+
+  private def _get[T](box: Box[T]): T = writes.get(box.asInstanceOf[Box[_]]).asInstanceOf[Option[T]].getOrElse(base.valueOf(box).getOrElse(
+    throw new RuntimeException("Missing Box for id " + box.id)
+  ))
+
+  def set[T](box: Box[T], t: T) = {
+    val different = _get(box) != t
+    //If box value would not be changed, skip write
+    val delta = if (different) {
+      this.copy(writes = writes.updated(box, t))
+    } else {
+      this
+    }
+    //TODO handle reactions
+    //      withReactor(_.afterSet(box, t, different))
+    (delta, box)
+  }
+
+  def get[T](box: Box[T]) = {
+    val v = _get(box)
+
+    //TODO handle reactions
+    //Only need to use a reactor if one is active
+    //      currentReactor.foreach(_.afterGet(box))
+    (this.copy(reads = reads + box.id), v)
+  }
+
+  override def toString = "RevisionDelta(" + base.toString + ")"
+}
+
+object RevisionDelta {
+  def apply(revision: Revision): RevisionDelta = RevisionDelta(
+    revision,
+    //Empty creates, reads, writes and new reactions
+    Set.empty,
+    Set.empty,
+    Map.empty,
+    Map.empty,
+    //Inherit reaction sources and targets, and boxes retaining reactions, from revision
+    revision.sources,
+    revision.targets,
+    revision.boxReactions)
+}
+
+class Revision(val index: Long, val map: Map[Long, BoxChange], reactionMap: Map[Long, State[RevisionDelta, Unit]], val sources: BiMultiMap[Long, Long], val targets: BiMultiMap[Long, Long], val boxReactions: Map[Long, Set[Reaction]]) {
+
+  def stateOf[T](box: Box[T]): Option[BoxState[T]] = for {
+    change <- map.get(box.id)
+    value <- box.getValue(change)
+  } yield BoxState(change.revision, value)
+
+  def indexOf(box: Box[_]): Option[Long] = map.get(box.id).map(_.revision)
+
+  def valueOf[T](box: Box[T]): Option[T] = {
+    val change = map.get(box.id)
+    change.flatMap(c => box.getValue(c))
+  }
+
+  private def indexOfId(id: Long): Option[Long] = map.get(id).map(_.revision)
+
+  private def reactionOfId(id: Long) = reactionMap.get(id)
+
+  private[core] def updated(delta: RevisionDelta, deletes: List[Long], reactionDeletes: List[Long]) = {
+    val newIndex = index + 1
+
+    //Remove boxes that have been GCed, then add new ones
+    val prunedMap = deletes.foldLeft(map) { case (m, id) => m - id }
+
+    val newMap = delta.writes.foldLeft(prunedMap) { case (m, (box, value)) =>
+      val newChange = BoxChange(newIndex)
+      box.asInstanceOf[Box[Any]].addChange(newChange, value)
+      m.updated(box.id, newChange)
+    }
+
+    //Remove reactions that have been GCed, then add new ones
+    val prunedReactionMap = reactionDeletes.foldLeft(reactionMap) { case (m, id) => m - id }
+    val newReactionMap = delta.newReactions.foldLeft(prunedReactionMap) { case (m, (reaction, f)) => m.updated(reaction.id, f) }
+
+    //Where boxes have been GCed, also remove the entry in boxReactions for that box - we only want the
+    //boxReactions maps to retain reactions for revisions while the boxes are still reachable
+    val prunedBoxReactions = deletes.foldLeft(boxReactions) { case (m, id) => m - id }
+
+    //Do not track sources and targets of removed reactions
+    val prunedSources = sources.removedKeys(reactionDeletes.toSet)
+    val prunedTargets = targets.removedKeys(reactionDeletes.toSet)
+
+    new Revision(newIndex, newMap, newReactionMap, prunedSources, prunedTargets, prunedBoxReactions)
+  }
+
+  def conflictsWith(d: RevisionDelta) = {
+    val start = d.base.index
+    d.reads.iterator.flatMap(indexOfId).exists(_ > start) || d.writes.keysIterator.flatMap(indexOf).exists(_ > start)
+  }
+
+  override def toString = "Revision(" + index + ")"
+}
+
+object Shelf {
 
   private val lock = RWLock()
-  private var current = new RevisionMonad(0, Map.empty, Map.empty, BiMultiMap.empty, BiMultiMap.empty, Map.empty)
+  private var current = new Revision(0, Map.empty, Map.empty, BiMultiMap.empty, BiMultiMap.empty, Map.empty)
 
   private val retries = 10000
 
@@ -30,9 +172,9 @@ class ShelfMonad() {
 
   def currentRevisionDelta = RevisionDelta(currentRevision)
 
-  private def createBox[T]() = BoxMonad[T]()
+  private def createBox[T]() = Box[T]()
 
-  private def revise(updated: RevisionMonad) {
+  private def revise(updated: Revision) {
     current = updated
 
     //TODO this can be done outside the lock by just passing the new revision to a queue to be
@@ -41,7 +183,17 @@ class ShelfMonad() {
 //    autos.foreach(_.add(updated))
   }
 
-  def commit(delta: RevisionDelta): Option[RevisionMonad] = {
+  def run[A](s: State[RevisionDelta, A]): Option[(Revision, A)] = {
+    val (delta, a) = s.run(currentRevisionDelta)
+    commit(delta).map((_, a))
+  }
+
+  def runRepeated[A](s: State[RevisionDelta, A]): (Revision, A) =
+    Range(0, retries).view.map(_ => run(s)).find(o => o.isDefined).flatten.getOrElse(throw new RuntimeException("Transaction failed too many times"))
+
+  def atomic[A](s: State[RevisionDelta, A]): A = runRepeated(s)._2
+
+  def commit(delta: RevisionDelta): Option[Revision] = {
     lock.write{
       if (!current.conflictsWith(delta)) {
         //Watch new boxes, make new revision with GCed boxes deleted, and make the new updated revision
@@ -69,139 +221,4 @@ class ShelfMonad() {
       }
     }
   }
-
-  //Path-dependent types
-
-  private class BoxMonad[T](val id: Long) extends Box[T] {
-    /**
-     * Store changes to this box, as a map from the Change to the State that was
-     * set by that Change. Changes that form a revision are retained by RevisionMonad
-     * and used to look up States in refs.
-     * Since this is a weak map, it does not retain Changes, they are retained only
-     * by the revisions they are in.
-     * When a BoxMonad is GCed, the changes are also GCed, allowing the States
-     * of the Box (instances of T) to be removed.
-     */
-    private val changes = new WeakHashMap[BoxMonadChange, T]()
-
-    private[core] def addChange(c: BoxMonadChange, t: T) = changes.put(c, t)
-
-    private[core] def getValue(c: BoxMonadChange) = changes.get(c)
-
-    override def toString = "BoxMonad(id = " + id + ", changes = " + changes + ")"
-  }
-
-  private object BoxMonad {
-    private val nextId = new AtomicInteger(0)
-
-    def apply[T](): Box[T] = new BoxMonad[T](nextId.getAndIncrement())
-  }
-
-  private case class ReactionMonad(val id: Long) extends Reaction
-
-  private object ReactionMonad {
-    private val nextId = new AtomicInteger(0)
-
-    def apply(): ReactionMonad = ReactionMonad(nextId.getAndIncrement())
-  }
-
-  case class BoxMonadChange(val revision: Long)
-
-  case class RevisionDelta(
-                            base: RevisionMonad,
-                            creates: Set[Box[_]],
-                            reads: Set[Long],
-                            writes: Map[Box[_], _],
-                            newReactions: Map[Reaction, ReactionFunc],
-                            sources: BiMultiMap[Long, Long],
-                            targets: BiMultiMap[Long, Long],
-                            boxReactions: Map[Long, Set[Reaction]]) {
-
-    def create[T](t: T) = {
-      val box = BoxMonad[T]()
-      (this.copy(creates = creates + box).copy(writes = writes.updated(box, t)), box)
-    }
-
-    private def _get[T](box: BoxR[T]): T = writes.get(box.asInstanceOf[Box[_]]).asInstanceOf[Option[T]].getOrElse(base.valueOf(box).getOrElse(
-      throw new RuntimeException("Missing Box for id " + box.id)
-    ))
-
-    def set[T](box: Box[T], t: T) = {
-      val different = _get(box) != t
-      //If box value would not be changed, skip write
-      val delta = if (different) {
-        this.copy(writes = writes.updated(box, t))
-      } else {
-        this
-      }
-      //TODO handle reactions
-//      withReactor(_.afterSet(box, t, different))
-      (delta, box)
-    }
-
-    def get[T](box: BoxR[T]) = {
-      val v = _get(box)
-
-      //TODO handle reactions
-      //Only need to use a reactor if one is active
-//      currentReactor.foreach(_.afterGet(box))
-      (this.copy(reads = reads + box.id), v)
-    }
-  }
-
-  object RevisionDelta {
-    def apply(revision: RevisionMonad): RevisionDelta = RevisionDelta(revision, Set.empty, Set.empty, Map.empty, Map.empty, BiMultiMap.empty, BiMultiMap.empty, Map.empty)
-  }
-
-  class RevisionMonad(val index: Long, val map: Map[Long, BoxMonadChange], reactionMap: Map[Long, ReactionFunc], val sources: BiMultiMap[Long, Long], val targets: BiMultiMap[Long, Long], val boxReactions: Map[Long, Set[Reaction]]) {
-
-    def stateOf[T](box: BoxR[T]): Option[BoxState[T]] = for {
-      change <- map.get(box.id)
-      value <- box.asInstanceOf[BoxMonad[T]].getValue(change)
-    } yield BoxState(change.revision, value)
-
-    def indexOf(box: BoxR[_]): Option[Long] = map.get(box.id).map(_.revision)
-
-    def valueOf[T](box: BoxR[T]): Option[T] = {
-      val change = map.get(box.id)
-      change.flatMap(c => box.asInstanceOf[BoxMonad[T]].getValue(c))
-    }
-
-    private def indexOfId(id: Long): Option[Long] = map.get(id).map(_.revision)
-
-    private def reactionOfId(id: Long) = reactionMap.get(id)
-
-    def updated(delta: RevisionDelta, deletes: List[Long], reactionDeletes: List[Long]) = {
-      val newIndex = index + 1
-
-      //Remove boxes that have been GCed, then add new ones
-      val prunedMap = deletes.foldLeft(map) { case (m, id) => m - id }
-
-      val newMap = delta.writes.foldLeft(prunedMap) { case (m, (box, value)) =>
-        val newChange = BoxMonadChange(newIndex)
-        box.asInstanceOf[BoxMonad[Any]].addChange(newChange, value)
-        m.updated(box.id, newChange)
-      }
-
-      //Remove reactions that have been GCed, then add new ones
-      val prunedReactionMap = reactionDeletes.foldLeft(reactionMap) { case (m, id) => m - id }
-      val newReactionMap = delta.newReactions.foldLeft(prunedReactionMap) { case (m, (reaction, f)) => m.updated(reaction.id, f) }
-
-      //Where boxes have been GCed, also remove the entry in boxReactions for that box - we only want the
-      //boxReactions maps to retain reactions for revisions while the boxes are still reachable
-      val prunedBoxReactions = deletes.foldLeft(boxReactions) { case (m, id) => m - id }
-
-      //Do not track sources and targets of removed reactions
-      val prunedSources = sources.removedKeys(reactionDeletes.toSet)
-      val prunedTargets = targets.removedKeys(reactionDeletes.toSet)
-
-      new RevisionMonad(newIndex, newMap, newReactionMap, prunedSources, prunedTargets, prunedBoxReactions)
-    }
-
-    def conflictsWith(d: RevisionDelta) = {
-      val start = d.base.index
-      d.reads.iterator.flatMap(indexOfId).exists(_ > start) || d.writes.keysIterator.flatMap(indexOf).exists(_ > start)
-    }
-  }
-
 }
---
+++ src/main/scala/org/rebeam/boxes/core/monad/ShelfActions.scala
@@ -0,0 +1,10 @@
+package org.rebeam.boxes.core.monad
+
+import scalaz._
+
+object ShelfActions {
+  def create[T](t: T): State[RevisionDelta, Box[T]] = State(_.create(t))
+  def set[T](box: Box[T], t: T): State[RevisionDelta, Box[T]] = State(_.set(box, t))
+  def get[T](box: Box[T]): State[RevisionDelta, T] = State(_.get(box))
+  def atomic[A](s: State[RevisionDelta, A]): A = Shelf.atomic(s)
+}
