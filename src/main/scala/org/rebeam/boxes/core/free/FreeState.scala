9133a61f8d7077198bd33ede16b0b221f94d9109
trepidacious <trepidacious@gmail.com>
2015-08-15 12:32:57 +0100

    Pull in some example Free stuff
    Just temporarily, to assess different approaches.

---
+++ src/main/scala/org/rebeam/boxes/core/free/FreeState.scala
@@ -0,0 +1,170 @@
+package org.rebeam.boxes.core.free
+
+import scala.language.higherKinds
+import scala.annotation.tailrec
+import scalaz._
+import scalaz.Scalaz._
+import scalaz.Free._
+
+trait EffectWorld {
+
+  /** Our world's state. Subclasses may choose to make this type public. */
+  protected type State
+
+  /** An action in this `World`, returning a result of type `A`. */
+  type Action[A]
+
+  /** Run an `Action` with some initial `State`, returning the final state and result. */
+  protected def runWorld[A](a: Action[A], w: State): (State, A)
+
+  /** Construct an `Action` for a computation that transitions the `State` and produces a result of type `A`. */
+  protected def action[A](f: State => (State, A)): Action[A]
+
+  /** Construct an `Action` for a computation consumes the `State` and produces a result of type `A`. */
+  protected final def effect[A](f: State => A): Action[A] =
+    action(s => (s, f(s)))
+
+  /** Construct an `Action` for a computation that simply returns a value of type `A`. */
+  protected final def unit[A](a: => A): Action[A] =
+    action(s => (s, a))
+
+}
+
+/** An `EffectWorld` implemented in terms of `Free`. */
+trait FreeWorld extends EffectWorld {
+
+  /** Our operations are simple state transitions. */
+  case class Op[+A] private[FreeWorld] (f: State => (State, A))
+
+  /** Operation must have a `Functor` in order to gain a `Free` monad. */
+  implicit val OpFunctor: Functor[Op] =
+    new Functor[Op] {
+      def map[A, B](op: Op[A])(g: A => B) = //Op(op.f(_).rightMap(g))
+      //Equivalent to the following - rightMap applies g to the right element of the (newState, a) pair.
+        Op(state => {
+          val (newState, a) = op.f(state)
+          (newState, g(a))
+        })
+    }
+
+  type Action[A] = Free[Op, A]
+
+  protected def action[A](f: State => (State, A)): Action[A] = liftF(Op(f))
+
+  @tailrec protected final def runWorld[A](a: Action[A], s: State): (State, A) =
+    a.resume match { // N.B. resume.fold() doesn't permit TCO
+      case -\/(Op(f)) =>
+        val (s0, a) = f(s)
+        runWorld(a, s0)
+      case \/-(a) => (s, a)
+    }
+
+}
+
+/** An `EffectWorld` implemented in terms of `Trampoline`. */
+trait World extends EffectWorld {
+
+  class Action[A] protected[World] (private[World] val t: State => Trampoline[(State, A)]) {
+    def map[B](f: A => B): Action[B] =
+      new Action(w => for { p <- t(w) } yield (p._1, f(p._2)))
+    def flatMap[B](f: A => Action[B]): Action[B] =
+      new Action(w => for { p <- t(w); x <- f(p._2).t(w) } yield x)
+  }
+
+  object Action {
+
+    implicit object ActionMonad extends Monad[Action] {
+      def point[A](a: => A): Action[A] =
+        action(w => (w, a))
+      override def map[A, B](fa: Action[A])(f: (A) => B) =
+        fa map f
+      def bind[A, B](fa: Action[A])(f: (A) => Action[B]): Action[B] =
+        fa flatMap f
+    }
+
+  }
+
+  protected def runWorld[A](a: Action[A], w: State): (State, A) =
+    a.t(w).run
+
+  protected def action[A](f: State => (State, A)): Action[A] =
+    new Action(w => return_(f(w)))
+
+}
+
+/** An `EffectWorld` implemented in terms of `Trampoline`. */
+trait FixedWorld extends EffectWorld {
+
+  class Action[A] protected[FixedWorld] (private[FixedWorld] val t: State => Trampoline[(State, A)]) {
+    def map[B](f: A => B): Action[B] =
+      new Action(w => for { p <- t(w) } yield (p._1, f(p._2)))
+    def flatMap[B](f: A => Action[B]): Action[B] =
+      new Action(w => for { p <- t(w); x <- f(p._2).t(p._1) } yield x)
+  }
+
+  object Action {
+
+    implicit object ActionMonad extends Monad[Action] {
+      def point[A](a: => A): Action[A] =
+        action(w => (w, a))
+      override def map[A, B](fa: Action[A])(f: (A) => B) =
+        fa map f
+      def bind[A, B](fa: Action[A])(f: (A) => Action[B]): Action[B] =
+        fa flatMap f
+    }
+
+  }
+
+  protected def runWorld[A](a: Action[A], w: State): (State, A) =
+    a.t(w).run
+
+  protected def action[A](f: State => (State, A)): Action[A] =
+    new Action(w => return_(f(w)))
+
+}
+
+object FreeMapWorld extends FreeWorld with App {
+  type State = Map[String, String]
+
+  val s: State = Map.empty
+
+  def set(key: String, value: String): Action[Unit] = action((s: State) => {
+    val newState = s.updated(key, value)
+    (newState, ())
+  })
+
+  def get(key: String): Action[String] = action((s: State) => {
+    (s, s.apply(key))
+  })
+
+  val test = for {
+    _ <- set("a", "1")
+    a <- get("a")
+    _ <- set("b", a)
+  } yield (a)
+
+  println(runWorld(test, s))
+}
+
+object MapWorld extends FixedWorld with App {
+  type State = Map[String, String]
+
+  val s: State = Map.empty
+
+  def set(key: String, value: String): Action[Unit] = action((s: State) => {
+    val newState = s.updated(key, value)
+    (newState, ())
+  })
+
+  def get(key: String): Action[String] = action((s: State) => {
+    (s, s.apply(key))
+  })
+
+  val test = for {
+    _ <- set("a", "1")
+    a <- get("a")
+    _ <- set("b", a)
+  } yield (a)
+
+  println(runWorld(test, s))
+}
---
+++ src/main/scala/org/rebeam/boxes/core/free/KVS.scala
@@ -0,0 +1,93 @@
+package org.rebeam.boxes.core.free
+
+import scalaz._
+import Scalaz._
+import Free._
+import scala.collection.mutable
+
+// This example is based off the one in Runar Bjarnason's "Dead Simple Dependency Injection" talk.
+// http://www.youtube.com/watch?v=ZasXwtTRkio
+
+
+// 0. Fantasy API
+// def put(key: String, value: String): Unit
+// def get(key: String): String
+// def delete(key: String): Unit
+
+
+// 1. ADT
+sealed trait KVS[+Next]
+case class Put[Next](key: String, value: String, next: Next) extends KVS[Next]     // <----  def put(key: String, value: String): Unit
+case class Get[Next](key: String, onResult: String => Next) extends KVS[Next]      // <----  def get(key: String): String
+case class Delete[Next](key: String, next: Next) extends KVS[Next]                 // <----  def delete(key: String): Unit
+
+
+object KVS {
+  type Script[A] = Free[KVS, A]
+
+  // 2. Functor definition
+  implicit val functor: Functor[KVS] = new Functor[KVS] {
+    def map[A,B](kvs: KVS[A])(f: A => B): KVS[B] = kvs match {
+      case Put(key, value, next) => Put(key, value, f(next))
+      case Get(key, onResult) => Get(key, onResult andThen f)
+      case Delete(key, next) => Delete(key, f(next))
+    }
+  }
+
+//  /** Return from the computation with the given value. */
+//  private[scalaz] case class Return[S[_], A](a: A) extends Free[S, A]
+//
+//  /** Suspend the computation with the given suspension. */
+//  private[scalaz] case class Suspend[S[_], A](a: S[Free[S, A]]) extends Free[S, A]
+//
+//  def liftF[S[_], A](value: => S[A])(implicit S: Functor[S]): Free[S, A] =
+//    Suspend(S.map(value)(Return[S, A]))
+
+  // 3. Lifting functions
+  def put(key: String, value: String): Script[Unit] = liftF( Put(key, value, ()) )  //We liftF S[A] an value (i.e. KVS[Unit]) into corresponding Free[S, A] (i.e. Free[KVS, Unit] or equivalently Script[Unit). This is because Put(key, value, ()) is a KVS[Unit]
+
+  def get(key: String): Script[String] = liftF(Get(key, identity))                  //And now liftF from KVS[String] into Free[KVS, String] or equivalently Script[String]. Here Get(key, identity) is a Get[String] since identity will be String => String.
+
+  def delete(key: String): Script[Unit] = liftF(Delete(key, ()))                    //Another like put, since we don't return a value.
+
+
+  // 4. Composite functions
+  def modify(key: String, f: String => String): Free[KVS, Unit] = for {
+    v <- get(key)
+    _ <- put(key, f(v))
+  } yield ()
+
+
+  // 5. Write scripts
+  val script: Free[KVS, Unit] = for {
+    id <- get("swiss-bank-account-id")
+    _ <- modify(id, (_ + 1000000))
+    _ <- put("bermuda-airport", "getaway car")
+    _ <- delete("tax-records")
+  } yield ()
+
+
+  // 6. Interpreters
+
+  // Building an immutable structure
+  def interpretPure(kvs: Script[Unit], table: Map[String, String] = Map.empty): Map[String, String] = kvs.resume.fold({
+    case Get(key, onResult) => interpretPure(onResult(table(key)), table)
+    case Put(key, value, next) => interpretPure(next, table + (key -> value))
+    case Delete(key, next) => interpretPure(next, table - key)
+  }, _ => table)
+
+
+  // Directly running effects
+  def interpretImpure(kvs: Script[Unit], table: mutable.Map[String, String]): Unit = kvs.go {
+    case Get(key, onResult) => onResult(table(key))
+    case Put(key, value, next) => table += (key -> value); next
+    case Delete(key, next) => table -= key; next
+  }
+
+}
+
+object KVSApp extends App {
+  import KVS._
+  val table = interpretPure(script, Map("swiss-bank-account-id" -> "123412", "123412" -> "0"))
+  println(table)
+}
